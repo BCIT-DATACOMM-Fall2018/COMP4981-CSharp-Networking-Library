bd72d565db5f88787a501cfcfc0bcdc01bf77ebf
Cameron Roberts <cameronroberts@shaw.ca>
2019-03-16 00:41:37 -0700

    Add RequiresCollision property of AbilityInfo

--- NetworkLibrary/Source/AbilityInfo.cs
+++ NetworkLibrary/Source/AbilityInfo.cs
@@ -6,32 +6,43 @@ namespace NetworkLibrary
 	{
 		public static readonly AbilityInfo[] InfoArray = {
 			// TestProjectile
-			new AbilityInfo (isArea: true),
+			new AbilityInfo (isArea: true, requiresCollision: true),
 			// TestTargeted
 			new AbilityInfo (isTargeted: true, allyTargetAllowed: true, enemyTargetAllowed: true),
 			// TestHomingTargeted
-			new AbilityInfo (isTargeted: true, enemyTargetAllowed: true),
+			new AbilityInfo (isTargeted: true, enemyTargetAllowed: true, requiresCollision: true),
 			// TestAreaOfEffect
-			new AbilityInfo (isArea: true)
+			new AbilityInfo (isArea: true, requiresCollision: true)
 		};
 
+		// The ability targets a location on the map
 		public bool IsArea { get; private set; }
 
+		// The ability targets an actor
 		public bool IsTargeted{ get; private set; }
 
+		// The ability targets only the player using it
 		public bool IsSelf{ get; private set; }
 
+		// The ability is allowed to target allies (includes player)
 		public bool AllyTargetAllowed{ get; private set; }
 
+		// The ability is allowed to target enemies
 		public bool EnemyTargetAllowed{ get; private set; }
 
-		private AbilityInfo (bool isArea = false, bool isTargeted = false, bool isSelf = false, bool allyTargetAllowed = false, bool enemyTargetAllowed = false)
+		// The ability requires client calculation of collision. If this is false the server will
+		// apply the abilities effects immediatly upon receiving the packet.
+		public bool RequiresCollision{ get; private set; }
+
+
+		private AbilityInfo (bool isArea = false, bool isTargeted = false, bool isSelf = false, bool allyTargetAllowed = false, bool enemyTargetAllowed = false, bool requiresCollision = false)
 		{
 			IsArea = isArea;
 			IsTargeted = isTargeted;
 			IsSelf = isSelf;
 			AllyTargetAllowed = allyTargetAllowed;
 			EnemyTargetAllowed = enemyTargetAllowed;
+			RequiresCollision = requiresCollision;
 		}
 
 
